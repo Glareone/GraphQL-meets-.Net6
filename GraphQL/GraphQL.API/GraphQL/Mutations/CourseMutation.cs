ab2015c810902e464002fc2717d0b85f28ce6ef7
Glareone <enlarius@gmail.com>
2023-04-09 18:00:09 +0300

    single element mutation

--- GraphQL/GraphQL.API/GraphQL/Mutations/CourseMutation.cs
+++ GraphQL/GraphQL.API/GraphQL/Mutations/CourseMutation.cs
@@ -23,5 +23,23 @@ public sealed class CourseMutation: ObjectGraphType
                 // we return course here because in Repository method we return Course as result.
                 return repository.AddCourse(course);
             });
+        
+        Field<CourseType>(
+            "updateCourse",
+            "is used to update an existing course",
+            arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<IdGraphType>>
+                    { Name = "id", Description = "Id of the course you want to update" },
+                new QueryArgument<NonNullGraphType<CourseInputType>>
+                    { Name = "course", Description = "Updated course Values" }
+            ),
+            resolve: context =>
+            {
+                var id = context.GetArgument<int>("id");
+                var course = context.GetArgument<Course>("course");
+
+                return repository.UpdateCourse(id, course);
+            }
+        );
     }
 }
